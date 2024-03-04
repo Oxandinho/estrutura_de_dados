00fb22410d8638a592e417ee1a4e96bd01428986
Alexandro Debastiani Aguiar Brandt <adabrandt@furb.br>
2024-03-04 10:04:26 -0300

    Enviando arquivos

---
+++ src/exe1.java
@@ -0,0 +1,20 @@
+public class exe1 {
+    public static void main(String[] args) {
+
+        /*
+    Utilizando um array estático de dez posições, desenvolva um algoritmo com as seguintes funcionalidades:
+
+    Cadastrar;
+    Listar todos os nomes;
+    Alterar;
+    Remover;
+    Finalizar.
+
+	Essa estrutura ficará em um laço de repetição.
+
+     */
+        int [] funcionalidades = new int[10 ];
+        
+
+    }
+}
